72a42a8a3101c0fb50bbdf2b04a0f2b0b4a49d86
unknown <farhanayesmin550@gmail.com>
2023-09-29 22:49:34 +0600

    Modal c added

--- src/components/ModalA.jsx
+++ src/components/ModalA.jsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 const ModalA = ({ toggleA, toggleB, toggleC, onlyEven, setOnlyEven }) => {
--- src/components/ModalC.jsx
+++ src/components/ModalC.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
-const ModalC = () => {
-    return (
-        <div>
-            <h1>dfhgfhf</h1>
+const ModalC = ({ toggleC, contact }) => {
+  return (
+    <div className="modal" tabIndex="-1" style={{ display: 'block' }}>
+      <div className="modal-dialog modal-dialog-centered" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Modal C - {contact ? contact.name : ''}</h5>
+            <button type="button" className="close" aria-label="Close" onClick={toggleC}>
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            Modal C Content...
+            {contact && <p>{contact.details}</p>}
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-outline" style={{ border: "2px solid #46139f" }} onClick={toggleC}>Close</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ModalC;
